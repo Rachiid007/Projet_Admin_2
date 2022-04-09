722ad637f8c36af508c4a87c2a3e509f5cbc8d59
Rachiid007 <abderrachid.bellaali@gmail.com>
2022-04-09 18:42:32 +0200

    Web Public - test

---
+++ WEB_Public/Web-dmz/www/b2b/index.php
@@ -0,0 +1,26 @@
+<?php
+    $db = mysqli_connect('176.96.231.209:5000','woody_admin','admin','woodytoys_db');
+    or die('Error connecting to MySQL server.');
+?>
+
+<html>
+
+<head>
+</head>
+
+<body>
+    <h1>Bienvenu sur le site B2B (revendeurs)</h1>
+ 
+    <?php
+        $query = "SELECT * FROM toys";
+        mysqli_query($db, $query) or die('Error querying database.');
+        $result = mysqli_query($db, $query);
+
+        while ($row = mysqli_fetch_array($result)) {
+            echo $row['id_toys'] . ': ' . $row['toyName'] . ' ' . $row['toyPrice'] . ' <br />';
+        }
+    ?>
+
+</body>
+
+</html>
