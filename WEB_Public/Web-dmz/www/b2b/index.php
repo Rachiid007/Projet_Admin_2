dd16813c81c13c7fed7109c6c10bc83f66b4309e
Nepsylon <simon.kinet08@outlook.com>
2022-04-17 14:30:43 +0100

    Configuration web fonctionnelle

--- WEB_Public/Web-dmz/www/b2b/index.php
+++ WEB_Public/Web-dmz/www/b2b/index.php
@@ -1,14 +1,13 @@
 <?php
-$mysqli = new mysqli("176.96.231.209:5000",'woody_admin','admin','woodytoys_db');
+$mysqli = new mysqli("176.96.231.209:5000",'root','admin','woodytoys_db');
 
 // Check connection
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
-?>
 
-<!-- 176.96.231.209:5000 -->
+?>
 
 <html>
 
@@ -19,9 +18,9 @@ if ($mysqli -> connect_errno) {
     <h1>Bienvenu sur le site B2B (revendeurs)</h1>
  
     <?php
-        $query = "SELECT * FROM toys";
-        mysqli_query($db, $query) or die('Error querying database.');
-        $result = mysqli_query($db, $query);
+        $query = "SELECT * FROM toys;";
+        mysqli_query($mysqli, $query) or die('Error querying database.');
+        $result = mysqli_query($mysqli, $query);
 
         while ($row = mysqli_fetch_array($result)) {
             echo $row['id_toys'] . ': ' . $row['toyName'] . ' ' . $row['toyPrice'] . ' <br />';
